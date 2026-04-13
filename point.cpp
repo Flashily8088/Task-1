8546ff9d65a6b32773a4b4f88a4465695409e0c1
Elizaveta Ryabova <elizaveta.ryabova.04@inbox.ru>
2026-04-13 14:14:38 +0300

    Add files via upload

---
+++ point.cpp
@@ -0,0 +1,33 @@
+#include "point.h"
+
+bool point::operator==(point p)
+{
+	return (abs(this->x - p.x) < numeric_limits<double>::epsilon() && abs(this->y - p.y) < numeric_limits<double>::epsilon()
+		&& abs(this->z - p.z) < numeric_limits<double>::epsilon());
+}
+
+bool point::operator!=(point p)
+{
+	return !(*this == p);
+}
+
+point& point::operator=(point& p)
+{
+	if (*this == p) {
+		return *this;
+	}
+	x = p.x;
+	y = p.y;
+	z = p.z;
+	return *this;
+}
+
+double point::get_x() {
+	return x;
+}
+double point::get_y() {
+	return y;
+}
+double point::get_z() {
+	return z;
+}
---
+++ program.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include"windows.h"
+#include "triangle.h"
+using namespace std;
+/**
+	* @breaf создаёт точку
+	*  * return точку
+	*/
+point creation();
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	point A = creation();
+	point B = creation();
+	point C = creation();
+	A = B;
+	triangle T(A, B, C);
+	cout << "r = " << T.r() << endl;
+	cout << "R = " << T.R();
+	return 0;
+}
+point creation() {
+	double x = 0, y = 0, z = 0;
+	cout << "Point: ";
+	cin >> x >> y >> z;
+	return point(x, y, z);;
+}
