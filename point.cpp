725fdd2dd61d907c7ffeace7cb86ebae0f0d098e
Elizaveta Ryabova <elizaveta.ryabova.04@inbox.ru>
2026-05-07 12:03:12 +0300

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -1,5 +1,7 @@
 #include "point.h"
 
+point::point() : x(0), y(0), z(0) {}
+point::point(const double a, const double b, const double c) : x(a), y(b), z(c) {}
 bool point::operator==(const point p) const
 {
 	return (abs(this->x - p.x) < numeric_limits<double>::epsilon() && abs(this->y - p.y) < numeric_limits<double>::epsilon()
@@ -22,12 +24,12 @@ point& point::operator=(point& p)
 	return *this;
 }
 
-double point::get_x() {
+double point::get_x() const {
 	return x;
 }
-double point::get_y() {
+double point::get_y() const {
 	return y;
 }
-double point::get_z() {
+double point::get_z() const {
 	return z;
 }
